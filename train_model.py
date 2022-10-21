0ba5763d28a7976bf9aeedc35a31ef6df91380e8
Mantas Cepulkovskis <mantas.cepulkovskis@ibm.com>
2022-10-21 18:00:09 +0300

    Initial dummy code to pass flake8 tests for initial build

--- train_model.py
+++ train_model.py
@@ -1,10 +1,25 @@
-# Script to train machine learning model.
+"""Script to train machine learning model."""
 
+import pandas as pd
+from typing import List
 from sklearn.model_selection import train_test_split
+
+
+def process_data(
+    train: pd.DataFrame,
+    categorical_features: List[str],
+    label: str,
+    training: bool
+) -> tuple:
+    # Process the test data with the process_data function.
+    # TODO: Define output properly
+    pass
+
 
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
+data = None
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -22,7 +37,5 @@ cat_features = [
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
-
-# Proces the test data with the process_data function.
 
 # Train and save a model.
